dec5bafa748585a89e22e904c3c1a8a1ed2a2036
Jose Gamboa <joalgamro2010@gmail.com>
2021-10-18 10:05:30 -0600

    A: dias_entre() js

--- Asignacion_1a.js
+++ Asignacion_1a.js
@@ -151,10 +151,68 @@ function fecha_futura (date, days){
     return date
 }
 
+//E: Una fecha valida
+//S: Un valor booleano
+//D: Determina si d1 es una fecha mayor que d2
+function fechaMayor(date1,date2){
+	//Se compara el año
+    if (date1[0] > date2[0]){
+        return True
+	}
+    if (date1[0] == date2[0]){
+		//Si el año es igual, se compara el mes 
+        if (date1[1] > date2[1]){
+            return True
+		}
+        if (date1[1] == date2[1]){
+			//Si el mes es igual, se compara el día
+            if (date1[2] > date2[2]){
+				return true
+			}
+		}
+	}
+    return false
+}
+
+//E: Una fecha valida
+//S: Un numero entero
+//D: Calcula la cantidad de días transcurridos desde la fecha 1 hasta la fecha 2, sin importar cual sea mayor
+function dias_entre(date1, date2){
+    //Se determina si la fecha 1 es mayor que la fecha 2 
+    if (fechaMayor(date1, date2)){
+        //Si es mayor se hace el cambio para ordenar las fehcas
+        temp = date1
+        date1 = date2
+        date2 = temp
+	}
+    //Se inicializa la variable res es 0
+    res = 0
+    //En caso de que sean el mismo año
+    if (date1[0] == date2[0]){
+        res = dias_desde_primero_enero(date2) - dias_desde_primero_enero(date1)
+	}
+	//En caso de que sean años diferentes 
+    else{
+        //Se calcula la cantidad de dias de los años completos que hay entre las fechas
+        for (let year = date1[0]+1; year < date2[0]; year++) {
+            if (bisiesto(year))
+                res += 366
+            else
+                res += 365
+		}
+        //Se suman los dias de la fecha 1
+        if (bisiesto(date1[0]))
+            res += 366-dias_desde_primero_enero(date1)
+		else
+            res += 365-dias_desde_primero_enero(date1)
+        //Se suman los dias de la fecha 2
+        res += dias_desde_primero_enero(date2)
+	}
+	return res
+}
 
 
 //Pruebas
-
 console.log('---- Pruebas R0 ----');
 
 // ingresando dato de tipo string
@@ -265,6 +323,25 @@ console.log(fecha_futura([2019,2,28],1))
 //Caso 1 año exacto 
 console.log(fecha_futura([2018,1,1],365))
 //Caso 1 año exacto bisciesto 
-console.log(fecha_futura([2020,1,1],365))
+console.log(fecha_futura([2020,1,1],366))
 //Caso 2 años exactos 
 console.log(fecha_futura([2018,1,1],730))
+
+console.log("\n---- Pruebas R9 ----")
+
+//Caso 0 dias 
+console.log(dias_entre([2019,1,1],[2019,1,1]))
+//Caso 15 días mismo año
+console.log(dias_entre([2019,1,1],[2019,1,16]))
+//Caso 15 días diferente año
+console.log(dias_entre([2019,12,20],[2020,1,4]))
+//Caso año bisciesto
+console.log(dias_entre([2020,2,28],[2020,2,29]))
+//Caso año no bisciesto 
+console.log(dias_entre([2019,2,28],[2019,3,1]))
+//Caso 1 año exacto 
+console.log(dias_entre([2018,1,1],[2019,1,1]))
+//Caso 1 año exacto bisciesto 
+console.log(dias_entre([2020,1,1],[2021,1,1]))
+//Caso 2 años exactos 
+console.log(dias_entre([2018,1,1],[2020,1,1]))
